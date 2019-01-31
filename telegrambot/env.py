c35e4687c30ccb13a511e8f81dea5e487a60f93b
Fabiano Bizarro <fabianoasbizarro@gmail.com>
2019-01-30 23:53:13 -0200

    fix environment variables

--- telegrambot/env.py
+++ telegrambot/env.py
@@ -1,18 +1,26 @@
 import os
 import configparser
+from dotenv import load_dotenv
 
 # path = os.path.dirname(os.path.abspath(__file__)) + '/../.ini'
 
 _default_port = 8443
 
-config = configparser.ConfigParser()
-config.read('.ini')
+load_dotenv()
 
-settings = config['default']
+# config = configparser.ConfigParser()
+# config.read('.ini')
+# settings = config['default']
+# # getting the values from .ini file
+# API_KEY = settings['API_KEY']
+# PORT = int(os.environ.get('PORT', _default_port))
+# BOT_ENDPOINT = settings['ENDPOINT']
+# TELEGRAM_API_ENDPOINT = settings['API_TELEGRAM']
+# ENVIRONMENT = settings['ENV'] or 'development'
 
-# getting the values from .ini file
-API_KEY = settings['API_KEY']
+
+API_KEY = os.getenv('API_KEY')
 PORT = int(os.environ.get('PORT', _default_port))
-BOT_ENDPOINT = settings['ENDPOINT']
-TELEGRAM_API_ENDPOINT = settings['API_TELEGRAM']
-ENVIRONMENT = settings['ENV'] or 'development'
+BOT_ENDPOINT = os.getenv('ENDPOINT')
+TELEGRAM_API_ENDPOINT = os.getenv('API_TELEGRAM')
+ENVIRONMENT = os.getenv('ENV') or 'development'
